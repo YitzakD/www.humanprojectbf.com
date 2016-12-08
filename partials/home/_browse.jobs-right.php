47474fbd74821ad2f2d9fd95f8642110e9a220ce
Yitzak DEKPEMOU <yitzak01@gmail.com>
2016-12-08 14:17:53 +0000

    Emploie à la une -> Telecharger plaquette

--- partials/home/_browse.jobs-right.php
+++ partials/home/_browse.jobs-right.php
@@ -3,40 +3,12 @@
     <div class="dev-div hp-jb-div margin-bottoms-1x">
 
         <div class="text-size-2x margin-bottoms-zx-plus">
-            <span>Emploi à la une</span>
-            <span class="float-right">
-                <i class="fa fa-angle-left"></i>
-                <i class="fa fa-angle-right"></i>
-            </span>
+            <span>Télécharger la plaquette</span>
         </div>
 
         <div class="hp-right-joboffer-div dev-color-grey bordered">
 
-            <div class="blocked margin-left-zx margin-right-zx margin-bottoms-zx-plus">
-                <span class="inlined float-left text-size-1x"
-                      style="width: 70%!important;">
-                    <?= "Directeur commercial"; ?>
-                </span>
-                <span class="inlined float-left dev-bgc-danger dev-color-white text-size-zx align-center"
-                      style="padding: 3px; width: 25%!important;">
-                    New
-                </span>
-            </div>
 
-            <div class="margin-left-zx margin-right-zx margin-bottoms-zx">
-                <span class="blocked text-size-zx">
-                    <i class="fa fa-briefcase"></i>&nbsp;<?= WEBSITE_NAME; ?>
-                </span>
-                <span class="blocked text-size-zx">
-                    <i class="fa fa-map-marker"></i>&nbsp;<?= "localisation"; ?>
-                </span>
-            </div>
-
-            <div class="margin-left-zx margin-right-zx margin-bottom-lg">
-                <span class="blocked text-size-zx">
-                    <?= "On sait depuis longtemps que travailler avec du texte lisible et contenant du sens est source de distractions, et empêche de se concentrer sur la mise en page elle-même. L'avantage du Lorem Ipsum sur un texte générique comme 'Du texte. Du texte. Du texte.' "; ?>
-                </span>
-            </div>
 
             <div class="align-center margin-bottoms-zx">
                 <a href="#" class="btn-hp-primary text-unerderline text-size-zx text-uppercase">
