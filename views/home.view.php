b83d684bbd0d8c25a202e0ca34dcabf0541e4e1d
Yitzak DEKPEMOU <yitzak01@gmail.com>
2016-12-09 12:07:03 +0000

    Content Edit -> Suppression temoignages contre gallerie

--- views/home.view.php
+++ views/home.view.php
@@ -16,16 +16,15 @@
     <div class="clearer margin-top-lg"></div>
 
     <div class="dev-col-max-big-plus no-margin relatived">
-        <div class="hp-block no-margin dev-bgc-danger">
-            <?php include "partials/home/_browse.jobs-left.php"; ?>
-            <?php include "partials/home/_browse.jobs-right.php"; ?>
+        <div class="hp-block no-margin">
+            <div class="dev-col-min-big float-left">
+              <?php include "partials/home/_browse.jobs-left.php"; ?>
+              <?php include "partials/home/_browse.testimonials.php"; ?>
+            </div>
+            <div class="dev-col-max-small float-right">
+              <?php include "partials/home/_browse.jobs-right.php"; ?>
+            </div>
         </div>
-    </div>
-
-    <div class="clearer margin-top-2x"></div>
-
-    <div class="dev-col-max-big-plus no-margin relatived">
-        <?php include "partials/home/_browse.testimonials.php"; ?>
     </div>
 
     <div class="clearer margin-top-2x"></div>
