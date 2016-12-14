28c7bd56d9f5a4c97387e2a2f85ae7ceb5f68b69
Yitzak DEKPEMOU <yitzak01@gmail.com>
2016-12-14 11:43:15 +0000

    Contents Edit -> Petite modification

--- partials/aboutus/_goals.php
+++ partials/aboutus/_goals.php
@@ -1,10 +1,10 @@
-<div class="hp-big-block dev-bgc-green-dev blocked">
+<div class="hp-big-block blocked">
 
   <div class="hp-inner-block-sect blocked no-margin" id="_goalsAnchor">
 
       <div class="hp-inner-sect-top-div no-margin"></div>
 
-      <div class="hp-inner-sect-middle-div no-margin dev-color-white">
+      <div class="hp-inner-sect-middle-div no-margin">
 
           <div class="dev-col-max-small margin-right-zx">Image d'illustration ici</div>
           <div class="dev-col-min-big">OBJECTIFS | Texte ici</div>
--- partials/aboutus/_presentation.php
+++ partials/aboutus/_presentation.php
@@ -1,12 +1,13 @@
-<div class="hp-big-block dev-bgc-green-dev blocked">
+<div class="hp-big-block blocked">
 
     <div class="hp-inner-block-sect blocked no-margin" id="_presentationAnchor">
 
         <div class="hp-inner-sect-top-div no-margin"></div>
 
-        <div class="hp-inner-sect-middle-div no-margin dev-color-white">
+        <div class="hp-inner-sect-middle-div no-margin">
 
             <div class="dev-div">PRESENETATION | Texte ici</div>
+            <div class="dev-div">Image d'illustration ici</div>
 
         </div>
 
